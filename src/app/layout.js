7f85c81438f5762e89a5d7d3619872d865133548
Santiago <santiprom84@gmai.com>
2026-08-14 12:18:12 -0300

    Corregir CartProvider en el layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { CartProvider } from '@/context/CartContext';
 
 export const metadata = {
   title: 'Polirubro Online.cba | Córdoba Capital',
@@ -11,12 +12,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body className="bg-gray-50 flex flex-col min-h-screen">
-        <Navbar />
-        {/* El main toma el espacio disponible para empujar el footer hacia abajo */}
-        <main className="flex-grow">
-          {children}
-        </main>
-        <Footer />
+        {/* Envolvemos todo en el CartProvider para que el carrito funcione */}
+        <CartProvider>
+          <Navbar />
+          <main className="flex-grow">
+            {children}
+          </main>
+          <Footer />
+        </CartProvider>
       </body>
     </html>
   );
